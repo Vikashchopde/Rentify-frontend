60772e864ec3de51ec060007abf50869dc3b4aae
vikas chopde <vikaschopde2000@gmail.com>
2026-05-27 09:17:13 +0530

    review

--- src/components/ReviewUser/ReviewForm.jsx
+++ src/components/ReviewUser/ReviewForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
-import useAuth from "../context/AuthContext";
+import useAuth from "../../context/AuthContext.jsx";
 
 export default function ReviewForm({ listingId, onSuccess }) {
   const [rating, setRating] = useState("");
